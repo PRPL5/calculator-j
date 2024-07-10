28d0d5d15390fbe1b37acb628b39ae83956641b4
PRPL5 <86297717+PRPL5@users.noreply.github.com>
2024-07-10 20:22:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let insert = document.getElementById('result');
+
+
+function display(val){
+    insert.value += val;
+     
+
+}
+
+
+function equal(){
+    
+    
+    document.getElementById('result').value = eval(insert.value);   
+    
+}
+function c(){
+    insert.value = ''
+}
+function ce(){
+insert.slice
+    
+}
+
